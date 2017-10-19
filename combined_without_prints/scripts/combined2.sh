3e5350f84068c4dfbc427f0817dc5476393d7047
Gauri Joshi <gpjoshi30@gatech.edu>
2017-10-19 15:14:38 -0400

    changes

--- combined_without_prints/scripts/combined2.sh
+++ combined_without_prints/scripts/combined2.sh
@@ -6,6 +6,6 @@
 #PBS -l nodes=2:sixcore
 for n2 in {2..12}
 do
-	OMPI_MCA_mpi_yield_when_idle=0 /opt/openmpi-1.4.3-gcc44/bin/mpirun -np 2 /nethome/pchoudhary32/CS6210_Project2/combined_without_prints/combined 1000 $n2 >> /nethome/pchoudhary32/CS6210_Project2/combined_without_prints/results/logc.txt
+	OMPI_MCA_mpi_yield_when_idle=0 mpirun -np 2 /nethome/gjoshi30/CS6210_Project2/combined_without_prints/combined 1000 $n2 >> /nethome/gjoshi30/CS6210_Project2/combined_without_prints/results/logc2.txt
 done
 #eof
